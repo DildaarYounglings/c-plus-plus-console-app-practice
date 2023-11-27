7c839c6f137929336323d665fd42fd028e851e5f
primedildaar2 <primedildaar2@gmail.com>
2023-11-27 11:48:27 +0200

    made notes on string methods in c++

--- CPlusPlusConsoleAppPractice/CPlusPlusConsoleAppPractice.cpp
+++ CPlusPlusConsoleAppPractice/CPlusPlusConsoleAppPractice.cpp
@@ -4,31 +4,36 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <string>
 #include <list>
-
+// useful string methods //
 int main() {
-	double temp;
-	char unit;
-	std::cout << "********************************************* Temperature Conversion Programe ******************************************";
-	std::cout << "F = Fahrenheit\n";
-	std::cout << "C = Celsius\n";
-	std::cout << "what woud you like to convert to: ";
-	std::cin >> unit;
-	if(unit == 'F' || unit == 'f'){
-		std::cout << "Enter the temperature in Celsius: ";
-		std::cin >> temp;
-		temp = (1.8 * temp) + 32.0;
-		std::cout << "Temperature is: " << temp << "F\n";
+	std::string name;
+	std::cout << "Enter your name: ";
+	std::getline(std::cin, name);
+	// empty method will return a bool value based on if your string is empty or not //
+	if (name.empty()) {
+		std::cout << "you didnt type a name even for the string";
 	}
-	else if(unit == 'C' || unit == 'c'){
-		std::cout << "Enter the temperature in Fahrenheit: ";
-		std::cin >> temp;
-		temp = (temp - 32) / 1.8;
-		std::cout << "Temperature is: " << temp << "C\n";
+	//	at method returns a character from a index specified in the string	//
+	name.at(0);
+	//	append method alsws add of a string to another string	//
+	name.append("r");
+	//	insert method adds a string at the specified index	//
+	name.insert(
+		/*index of where yo want to insert*/
+			0,
+		/*value that you want to insert in this string*/
+		"D"
+	);
+	std::cout << "your name is now " << name;
+	//	length method will get you the length of a string 	//
+	if (name.length() > 12) {
+		std::cout << "your name " << name;
+		std::cout << " is more tham 12 characters. here is the character count of the string: " << name.length() << "\n"; 
 	}
-	else{
-		std::cout << "please enter 'C','c','F' or 'f'";
-	}
+	//	clear method will delete every character in the string //
+	name.clear();
 }
 
 
